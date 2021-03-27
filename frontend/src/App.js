e53a79b0c8065f09c76348b8e787a719dfd88088
avpimblesr <allan.pimble@gmail.com>
2021-03-27 00:30:54 -0400

    Added starter js and css files

---
+++ frontend/src/App.js
@@ -0,0 +1,35 @@
+import { Route, Switch } from 'react-router-dom'
+import Navigation from './components/Navigation'
+import LoginForm from './components/LoginForm'
+import Forecast from './components/forecast/Forecast'
+import RegistrationForm from './components/RegistrationForm'
+
+import Container from '@material-ui/core/Container'
+
+import './App.css'
+
+const App = () => {
+  return (
+    <Container>
+      <Navigation position='fixed' />
+
+      <section className='console'>
+        <Switch>
+          <Route exact path='/'>
+            <LoginForm />
+          </Route>
+
+          <Route path='/forecast'>
+            <Forecast />
+          </Route>
+
+          <Route path='/registration'>
+            <RegistrationForm />
+          </Route>
+        </Switch>
+      </section>
+    </Container>
+  )
+}
+
+export default App
