a2f28f68acfa143bf525961d99f2609111ce45f9
avpimblesr <allan.pimble@gmail.com>
2021-03-27 01:01:30 -0400

    Added App.css and baseURL.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import { Route, Switch } from "react-router-dom";
+import Navigation from "./components/Navigation";
+import LoginForm from "./components/LoginForm";
+import Forecast from "./components/forecast/Forecast";
+import RegistrationForm from "./components/RegistrationForm";
+
+import Container from "@material-ui/core/Container";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Starting here</h1>
-    </div>
+    <Container maxWidth="sm">
+      <Navigation position="fixed" />
+
+      <section className="console">
+        <Switch>
+          <Route exact path="/">
+            <LoginForm />
+          </Route>
+
+          <Route path="/forecast">
+            <Forecast />
+          </Route>
+
+          <Route path="/registration">
+            <RegistrationForm />
+          </Route>
+        </Switch>
+      </section>
+    </Container>
   );
 }
 
